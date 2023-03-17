c0aab137e0058c6054a216b4872a246df3defa81
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-03-17 16:13:27 -0300

    Flyway & JPA implementation

--- src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -1,6 +1,5 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,23 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Client;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
 @RestController
 public class ClientController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clients")
 	public List<Client> list() {
-		Client client = new Client();
-		client.setId(1L);
-		client.setName("Gui");
-		client.setPhone("9129129");
-		client.setEmail("teste@gmail.com");
-		
-		Client client2 = new Client();
-		client2.setId(2L);
-		client2.setName("Oitudobom");
-		client2.setPhone("3211212121");
-		client2.setEmail("oiaham@gmail.com");
-		
-		return Arrays.asList(client, client2);
+		return manager.createQuery("from Client", Client.class).getResultList();
 	}
 }
--- src/main/java/com/algaworks/algalog/domain/model/Client.java
+++ src/main/java/com/algaworks/algalog/domain/model/Client.java
@@ -1,10 +1,24 @@
 package com.algaworks.algalog.domain.model;
 
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
+	
+	@Column(name = "fone")
 	private String phone;
 	
 	public Long getId() {
@@ -31,11 +45,28 @@ public class Client {
 		this.email = email;
 	}
 	
-	public String getPhone() {
+	public String getphone() {
 		return phone;
 	}
 	
-	public void setPhone(String phone) {
+	public void setphone(String phone) {
 		this.phone = phone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
 	}
 }
